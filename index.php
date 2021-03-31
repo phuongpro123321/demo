6111d5d08e94d7ed316278f38778358a4c479393
proselin <72499558+proselin@users.noreply.github.com>
2021-03-31 23:43:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php   $conn = pg_connect("host=ec2-23-22-191-232.compute-1.amazonaws.com dbname=ddpnh3fllikoh9 user=qlacuryvjwfagg password=2b902a01a3f536a9bc5398855594f01f3b6932ee4df7906b980e69d3726c4715 port=5432");
 if($conn){
 	echo 'done';
-}If($_REQUEST['RESQUES_METHOD'] = "POST"){	
+}
+if($_REQUEST['RESQUES_METHOD'] = "POST"){	
 	$username = $_POST['username'];
 	$password = $_POST['pass'];
 	$sql="SELECT * FROM account WHERE username ='{$username}' and pass='{$password}'";
