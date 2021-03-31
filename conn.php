b9af388acd14d1f0fe3d5acdd94a33d6b73df58c
proselin <72499558+proselin@users.noreply.github.com>
2021-03-31 23:08:26 +0700

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,9 +1,7 @@
 <?php 
 $conn_string = "host=ec2-23-22-191-232.compute-1.amazonaws.com dbname=ddpnh3fllikoh9 user=qlacuryvjwfagg password=2b902a01a3f536a9bc5398855594f01f3b6932ee4df7906b980e69d3726c4715";
 $dbconn = pg_connect($conn_string);
-if($dbconn){
-    echo 'do';
-}
+
 if(isset($_POST['username'])){
 	$username = $_POST['username'];
 }
