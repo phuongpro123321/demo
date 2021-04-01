981d6e2c25b49e7aaa0b2be1a2437cb717b0cf44
proselin <72499558+proselin@users.noreply.github.com>
2021-04-01 15:18:32 +0700

    Update index.php

--- index.php
+++ index.php
@@ -41,50 +41,19 @@ while ($row = pg_fetch_row($result)) {
 	<link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <body>
-<div class="container">
-	<div class="d-flex justify-content-center h-100">
+	<div class="container">
 		<div class="card">
-			<div class="card-header">
-				<h3>Sign In</h3>
-				<div class="d-flex justify-content-end social_icon">
-					<span><i class="fab fa-facebook-square"></i></span>
-					<span><i class="fab fa-google-plus-square"></i></span>
-					<span><i class="fab fa-twitter-square"></i></span>
-				</div>
-			</div>
-			<div class="card-body">
-				<form method="POST">
-					<div class="input-group form-group">
-						<div class="input-group-prepend">
-							<span class="input-group-text"><i class="fas fa-user"></i></span>
-						</div>
-						<input type="text" class="form-control" placeholder="username" name="username">
-						
-					</div>
-					<div class="input-group form-group">
-						<div class="input-group-prepend">
-							<span class="input-group-text"><i class="fas fa-key"></i></span>
-						</div>
-						<input type="password" class="form-control" placeholder="password" name="password">
-					</div>
-					<div class="row align-items-center remember">
-						<input type="checkbox">Remember Me
-					</div>
-					<div class="form-group">
-						<input type="submit" value="Login" class="btn float-right login_btn">
-					</div>
-				</form>
-			</div>
-			<div class="card-footer">
-				<div class="d-flex justify-content-center links">
-					Don't have an account?<a href="#">Sign Up</a>
-				</div>
-				<div class="d-flex justify-content-center">
-					<a href="#">Forgot your password?</a>
-				</div>
-			</div>
+			<h3>Sign In</h3>
 		</div>
+		<form method="POST">
+		<div class="card-body">
+			<label>username</label>
+			<input type="text" name="username">
+			<label>password</label>
+			<input type="password" name="password">
+			<input type="submit" value="login">
+		</div>
+		</form>
 	</div>
-</div>
 </body>
 </html>
