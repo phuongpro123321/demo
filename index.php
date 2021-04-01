df8311680cbc01bcc638da43c8e49bfb657e93d0
proselin <72499558+proselin@users.noreply.github.com>
2021-04-01 15:55:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php	 
  $conn = pg_connect("host=ec2-23-22-191-232.compute-1.amazonaws.com dbname=ddpnh3fllikoh9 user=qlacuryvjwfagg password=2b902a01a3f536a9bc5398855594f01f3b6932ee4df7906b980e69d3726c4715 port=5432");
- if($conn){echo "Connected";}
+ if(!$conn){echo "Lost Connect";}
  if(isset($_POST['login'])){
 	$username = $_POST['username'];
  	$password = $_POST['password'];
@@ -46,7 +46,7 @@
 			<input type="text" name="username">
 			<label>password</label>
 			<input type="password" name="password">
-			<input name="login" type="submit" value="login">
+			<button name="login" type="submit" value="login"></button>
 		</div>
 		</form>
 	</div>
